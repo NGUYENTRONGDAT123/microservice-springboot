cf806aae6f4a9e0758a1c35e272715e1b5302566
Trong Dat <datnguyentrong98@gmail.com>
2023-02-15 14:20:55 +1000

    initial commit

---
+++ src/main/java/com/tr_dat/productservice/ProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tr_dat.productservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tr_dat/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tr_dat.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
