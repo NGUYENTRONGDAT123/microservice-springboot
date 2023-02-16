411ec9987b15168a772508e731ead07bb8f8fbf8
Trong Dat <datnguyentrong98@gmail.com>
2023-02-16 16:58:08 +1000

    order service can talk with inventory service

--- inventory-service/src/main/java/com/tr_dat/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/tr_dat/inventoryservice/service/InventoryService.java
@@ -1,9 +1,12 @@
 package com.tr_dat.inventoryservice.service;
 
+import com.tr_dat.inventoryservice.dto.InventoryResponse;
 import com.tr_dat.inventoryservice.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -12,7 +15,13 @@ public class InventoryService {
     private final InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
-    public boolean isInStock(String skuCode) {
-        return inventoryRepository.findBySkuCode(skuCode).isPresent();
+    public List<InventoryResponse> isInStock(List<String> skuCode) {
+        return inventoryRepository.findBySkuCodeIn(skuCode).stream()
+          .map(inventory ->
+              InventoryResponse.builder()
+                .skuCode(inventory.getSkuCode())
+                .inStock(inventory.getQuantity() > 0)
+                .build()
+          ).toList();
     }
 }
